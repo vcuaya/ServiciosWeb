798f9177a0f1f926519a5e7322c8b9d84332ecc8
Víctor Cuaya <vcuaya@outlook.com>
2023-01-25 23:30:46 -0600

    Se corrigió la interfaz, ya que no se mostraban los tamaños correctos de la ventana y sus elementos

--- restclientAlumno/Form1.Designer.cs
+++ restclientAlumno/Form1.Designer.cs
@@ -70,7 +70,7 @@ namespace restclientAlumno
             // 
             this.textBox4.Location = new System.Drawing.Point(423, 32);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(125, 26);
+            this.textBox4.Size = new System.Drawing.Size(125, 20);
             this.textBox4.TabIndex = 4;
             // 
             // label1
@@ -78,7 +78,7 @@ namespace restclientAlumno
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Usuario";
             // 
@@ -87,7 +87,7 @@ namespace restclientAlumno
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(420, 16);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.Size = new System.Drawing.Size(51, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Semestre";
             // 
@@ -95,14 +95,14 @@ namespace restclientAlumno
             // 
             this.textBox1.Location = new System.Drawing.Point(9, 32);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 26);
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(285, 32);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(125, 26);
+            this.textBox3.Size = new System.Drawing.Size(125, 20);
             this.textBox3.TabIndex = 3;
             // 
             // label3
@@ -110,7 +110,7 @@ namespace restclientAlumno
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(282, 16);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.Size = new System.Drawing.Size(52, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Matrícula";
             // 
@@ -119,7 +119,7 @@ namespace restclientAlumno
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(144, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Password";
             // 
@@ -128,7 +128,7 @@ namespace restclientAlumno
             this.textBox2.Location = new System.Drawing.Point(147, 32);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(125, 26);
+            this.textBox2.Size = new System.Drawing.Size(125, 20);
             this.textBox2.TabIndex = 2;
             // 
             // groupBox2
@@ -156,9 +156,9 @@ namespace restclientAlumno
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Location = new System.Drawing.Point(6, 21);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(147, 20);
+            this.label5.Size = new System.Drawing.Size(98, 13);
             this.label5.TabIndex = 3;
             this.label5.Text = "Consulta disponible";
             // 
@@ -169,9 +169,10 @@ namespace restclientAlumno
             "Bienvenida",
             "Buscar Alumno",
             "Evaluar Alumno"});
-            this.comboBox1.Location = new System.Drawing.Point(6, 32);
+            this.comboBox1.Location = new System.Drawing.Point(9, 36);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 5;
             // 
             // textBox5
@@ -185,7 +186,7 @@ namespace restclientAlumno
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(586, 387);
             this.Controls.Add(this.textBox5);
@@ -193,6 +194,7 @@ namespace restclientAlumno
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
